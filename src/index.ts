54c9cf79004d78e79d8d06c3b91e61ccaff88934
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2023-09-24 23:02:44 +0530

    Add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => "Hello Elysia")
+  .get("/health", ({}) => {
+    return {
+      uptime: process.uptime(),
+      message: "OK",
+      timestamp: Date.now(),
+    };
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
